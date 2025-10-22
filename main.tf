21fc591a42021fc39987061a110a7bc03be8bb5c
Umar Farooque Khan <uffarooque73@gmail.com>
2025-10-23 00:40:03 +0530

    created role to connect through session manager

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.17.0"
     }
+  }
+}
+
+resource "aws_instance" "myTerraformInstance" {
+  ami                    = "ami-0341d95f75f311023"
+  instance_type          = "t2.micro"
+  iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
+  tags = {
+    Name = "MyTerraformInstanceNew"
   }
 }
